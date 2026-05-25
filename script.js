8d9f7e342dc079eb0d365aa83309bd37c578f9ba
Alex Jamshidi <alexjamshidi@gmail.com>
2026-05-25 20:30:33 +0100

    files updated

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+// ======================================================
+// ----- DOMS
+// ======================================================
+const episodes = document.getElementById("episodes");
+
+// ======================================================
+// ----- Runs on page load
+// ======================================================
+
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
   showAllEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+// ======================================================
+// ----- Episode Cards
+// ======================================================
 
+// Creates and displays episodes
 function showAllEpisodes(allEpisodes) {
   const episodeCards = allEpisodes.map(createEpisodeCard);
-  document.getElementById("episodes").append(...episodeCards);
+  episodes.append(...episodeCards);
 }
 
+// Creates single episode card
 function createEpisodeCard(episode) {
   const episodeCode = createEpisodeCode(episode);
   const episodeCard = document
     .getElementById("episode-card-template")
     .content.cloneNode(true);
-
   episodeCard.querySelector("img").src = episode.image.medium;
   episodeCard.querySelector("h3").textContent =
     `${episodeCode} - ${episode.name}`;
@@ -28,13 +36,14 @@ function createEpisodeCard(episode) {
   return episodeCard;
 }
 
+// creates episode code as a string e.g. S01E05
 function createEpisodeCode(episode) {
   const seasonNum = String(episode.season);
   const episodeNum = String(episode.number);
   return `S${seasonNum.padStart(2, 0)}E${episodeNum.padStart(2, 0)}`;
 }
 
-// =====================================================
+// ======================================================
 // ----- Page Loader
 // ======================================================
 window.onload = setup;
